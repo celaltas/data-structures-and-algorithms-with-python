c21e643bfab4ad9114f97cff4c65096adb3656e1
celal <celal.tas123@gmail.com>
2022-03-31 18:30:04 +0300

    data structures: queue

---
+++ queue.py
@@ -0,0 +1,16 @@
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
