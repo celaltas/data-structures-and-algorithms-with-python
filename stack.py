263a601b0433f7f3d6daec3c7aba5bccb7fdbb0b
celal <celal.tas123@gmail.com>
2022-03-31 18:10:01 +0300

    data structures: stack

---
+++ stack.py
@@ -0,0 +1,22 @@
+from inspect import stack
+
+
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def top(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
