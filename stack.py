bd2f8581b3e0b7e07c51531b8bfe4c9a67476436
celal <celal.tas123@gmail.com>
2022-04-01 23:30:33 +0300

    fixing .gitignore

--- stack.py
+++ stack.py
@@ -1,4 +1,4 @@
-from inspect import stack
+
 
 
 class Stack:
@@ -20,3 +20,61 @@ class Stack:
 
     def size(self):
         return len(self.items)
+
+
+"""
+Question: Stack Min: How would you design a stack which, in addition to push and pop, has a function min
+which returns the minimum element? Push, pop and min should all operate in 0(1) time.
+
+"""
+
+class StackWithMins(Stack):
+
+    def __init__(self, stack):
+        super().__init__()
+        self.stack = stack
+        
+
+    def pop(self):
+        if not self.is_empty():
+            if self.min() == self.items.pop():
+                self.stack.pop()
+
+
+    def push(self, item):
+        self.items.append(item)
+        if item<self.min():
+            self.stack.push(item)
+    
+    def min(self):
+        if self.stack.size()>0:
+            return self.stack.items[-1]
+        else:
+            return float("inf")
+
+
+stack = Stack()
+stack_with_mins = StackWithMins(stack=stack)
+
+
+stack_with_mins.push(6)
+print("min:", stack_with_mins.min())
+print("stack:", stack_with_mins.items)
+stack_with_mins.push(7)
+print("min:", stack_with_mins.min())
+print("stack:", stack_with_mins.items)
+stack_with_mins.push(5)
+print("min:", stack_with_mins.min())
+print("stack:", stack_with_mins.items)
+stack_with_mins.push(4)
+print("min:", stack_with_mins.min())
+print("stack:", stack_with_mins.items)
+stack_with_mins.pop()
+print("min:", stack_with_mins.min())
+print("stack:", stack_with_mins.items)
+stack_with_mins.pop()
+print("min:", stack_with_mins.min())
+print("stack:", stack_with_mins.items)
+stack_with_mins.pop()
+print("min:", stack_with_mins.min())
+print("stack:", stack_with_mins.items)
