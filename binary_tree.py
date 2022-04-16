970603adf6959e685c7bd80f54d99d0b01eabb92
celal <celal.tas123@gmail.com>
2022-04-16 15:09:08 +0300

    solved problems

--- binary_tree.py
+++ binary_tree.py
@@ -1,10 +1,13 @@
 
-
 class Node:
     def __init__(self, key):
         self.key = key
         self.left = None
         self.right = None
+
+    
+    def __repr__(self):
+        return f'{self.key}'
 
 
 def insert(root, key):
@@ -92,3 +95,88 @@ deleted_node = delete(root,6)
 print("deleted node:", deleted_node.key)
 inorder(root)
 
+"""
+
+Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an
+algorithm to create a binary search tree with minimal height.
+
+
+"""
+
+
+
+def create_min_bst(arr):
+    return create_min_height(arr, 0, len(arr)-1)
+
+
+def create_min_height(arr, start, end):
+    if end<start:
+        return None
+    
+    middle = (start+end)//2
+    node = Node(arr[middle])
+    node.left = create_min_height(arr, start, middle-1)
+    node.right = create_min_height(arr, middle+1, end)
+    return inorder(node)
+
+
+
+
+create_min_bst([4,5,6,7,8,9,10])
+
+
+
+
+"""
+
+List of Depths: Given a binary tree, design an algorithm which creates a linked list of all the nodes
+at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+
+
+"""
+
+
+
+def create_level_linked_list(root):
+    depth_lists =[]
+    depth = 0
+    specfiy_depth(root,depth_lists,depth)
+    return depth_lists
+
+
+
+def specfiy_depth(root,depth_lists,depth):
+
+    if root is None:
+        return None
+    
+    current_level_nodes = []
+
+    if len(depth_lists) == depth:
+        current_level_nodes = []
+        depth_lists.append(current_level_nodes)
+    else:
+        current_level_nodes = depth_lists[depth]
+    
+    current_level_nodes.append(root)
+    specfiy_depth(root.left,depth_lists,depth+1)
+    specfiy_depth(root.right,depth_lists,depth+1)
+
+
+root = None
+root = insert(root, 41)
+root = insert(root, 20)
+root = insert(root, 29)
+root = insert(root, 32)
+root = insert(root, 11)
+root = insert(root, 65)
+root = insert(root, 50)
+root = insert(root, 91)
+root = insert(root, 72)
+root = insert(root, 99)
+
+print("\nInorder traversal: ", end=' ')
+inorder(root)
+
+
+print(create_level_linked_list(root))
